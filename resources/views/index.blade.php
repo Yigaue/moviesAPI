b7f678c2f8c45a76fa718091ba3702bab9c70f0d
Gospel Lekia <gospellekia@Gospels-MacBook-Pro.local>
2020-05-06 00:21:02 +0100

    index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,51 @@
 @extends('layout.main')
 
 @section('content')
-<div>Index of Movies</div>
+<div class="container mx-auto px-4 pt-16">
+    <div class="popular-movies">
+        <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+            <div class="mt-8">
+                <a href="">
+                    <img src="/img/parasite'jpg" alt="parasite" class="hover:opacity-75 transistion ease-in-out duration-150">
+                </a>
+                <div class="mt-2">
+                    <a href="" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
+                    <div class="class flex items-center text-gray-400 text-sm mt-1">
+                        <span class="ml-1">star</span>
+                        <span class="mx-3">|</span>
+                        <span class="mx-3">85%</span>
+                        <span>may 02 2020</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Trailer, Comedy
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="now-playing-movies py-24">
+        <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Now Playing</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+            <div class="t-8">
+                <a href="">
+                    <img src="/img/parasite'jpg" alt="parasite" class="hover:opacity-75 transistion ease-in-out duration-150">
+                </a>
+                <div class="mt-2">
+                    <a href="" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
+                    <div class="class flex items-center text-gray-400 text-sm mt-1">
+                        <span class="ml-1">star</span>
+                        <span class="mx-3">|</span>
+                        <span class="mx-3">85%</span>
+                        <span>may 02 2020</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Trailer, Comedy
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
